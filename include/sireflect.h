550ba9f47853f3248d3a62fcdd026c6b512c03cd
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-06-08 19:53:53 +0200

    init project

---
+++ include/sireflect.h
@@ -0,0 +1,8 @@
+#ifndef SIREFLECT_H
+#define SIREFLECT_H
+
+/* This generated file contains includes for project dependencies */
+#include "sireflect/bake_config.h"
+
+#endif
+
---
+++ src/main.c
@@ -0,0 +1,5 @@
+#include <sireflect.h>
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
